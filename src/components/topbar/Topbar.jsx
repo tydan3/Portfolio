4517dc3539174e9349a9ee7bbb389bca57a90a6a
Daniel Ty <tydaniel270@gmail.com>
2021-11-20 15:48:57 -0800

    added navbar menu and material ui icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,26 @@
-import "./topbar.scss"
+import "./topbar.scss";
+import {Person, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
         <div className="topbar">
-            
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">genius.</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+1 (253) 391-0447</span>
+
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>tydaniel270@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    this is right
+                </div>
+            </div>
         </div>
     )
 }
