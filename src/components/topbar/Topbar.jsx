1365c7e20dea9666071db388c893251400e101cb
Daniel Ty <tydaniel270@gmail.com>
2021-11-22 17:07:05 -0800

    animated hamburger menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,15 @@
 import "./topbar.scss";
 import {Person, Mail} from "@material-ui/icons"
 
-export default function Topbar() {
+export default function Topbar({menuOpen, setMenuOpen}) {
     return (
-        <div className="topbar">
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">genius.</a>
                     <div className="itemContainer">
                         <Person className="icon"/>
                         <span>+1 (253) 391-0447</span>
-
                     </div>
                     <div className="itemContainer">
                         <Mail className="icon"/>
@@ -18,7 +17,11 @@ export default function Topbar() {
                     </div>
                 </div>
                 <div className="right">
-                    this is right
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
                 </div>
             </div>
         </div>
