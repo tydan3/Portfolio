c79c3c581762ccef394590bd2c305112919926f6
Daniel Ty <tydaniel270@gmail.com>
2021-11-27 07:26:12 -0800

    created intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,22 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/person.png" alt="man" />
+                </div>
+            </div>
+            
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I'm</h2>
+                    <h1>Daniel Ty</h1>
+                    <h3>Junior Dev<span></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="down" />
+                </a>
+            </div>
         </div>
     )
 }
