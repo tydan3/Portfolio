435049481c1695dad349dcd100ff6e10666d52b7
Daniel Ty <tydaniel270@gmail.com>
2021-11-29 02:22:08 -0800

    added text typing animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,19 @@
 import "./intro.scss"
+import {init} from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, { 
+            showCursor: true,
+            backDelay: 1500,
+            typeSpeed: 50,
+            backSpeed: 60,
+            strings: ['Developer', 'Engineer'] });
+    }, []);
+
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -13,7 +26,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi there, I'm</h2>
                     <h1>Daniel Ty</h1>
-                    <h3>Junior Dev<span></span></h3>
+                    <h3>Software <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                     <img src="assets/down.png" alt="down" />
