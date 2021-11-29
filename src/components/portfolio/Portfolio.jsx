4eed10463bbaa2efca250e76addecc2927afbfcc
Daniel Ty <tydaniel270@gmail.com>
2021-11-29 03:03:35 -0800

    created portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,40 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/wallpaper.jpg" alt="wallpaper" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/wallpaper.jpg" alt="wallpaper" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/wallpaper.jpg" alt="wallpaper" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/wallpaper.jpg" alt="wallpaper" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/wallpaper.jpg" alt="wallpaper" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/wallpaper.jpg" alt="wallpaper" />
+                    <h3>Banking App</h3>
+                </div>
+            </div>
         </div>
     )
 }
