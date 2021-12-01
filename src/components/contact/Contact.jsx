5973acc4b45026743ab450839caea76e5cc53df5
Daniel Ty <tydaniel270@gmail.com>
2021-12-01 04:24:52 -0800

    created contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
 import "./contact.scss";
 
 export default function Contact() {
-  return <div className="contact" id="contact"></div>;
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thank you, I will reply as soon as possible.</span>}
+        </form>
+      </div>
+    </div>
+  );
 }
