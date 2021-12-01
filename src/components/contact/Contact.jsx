8d4268b6d774b77dcf5409058d1e3ed908c6b1b9
Daniel Ty <tydaniel270@gmail.com>
2021-12-01 12:07:41 -0800

    added emailing functionality to contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,30 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./contact.scss";
+import emailjs from "emailjs-com";
 
 export default function Contact() {
   const [message, setMessage] = useState(false);
+  const formRef = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setMessage(true);
+
+    emailjs
+      .sendForm(
+        "service_natj6vo",
+        "template_zlo09df",
+        formRef.current,
+        "user_b6rhMytDWfjGBhWWtSzdp"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   return (
@@ -16,11 +34,16 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
+        <form ref={formRef} onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" name="user_email" />
+          <input type="text" placeholder="Subject" name="user_subject" />
+          <textarea placeholder="Message" name="user_message"></textarea>
           <button type="submit">Send</button>
-          {message && <span>Thank you, I will reply as soon as possible.</span>}
+          {message && (
+            <span>
+              Thanks for the message! I will reply as soon as possible.
+            </span>
+          )}
         </form>
       </div>
     </div>
