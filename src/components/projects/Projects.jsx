f76060fc54287f1d5cbbb7b01a204934377f4b1d
Daniel Ty <tydaniel270@gmail.com>
2021-12-08 04:57:15 -0800

    projects page responsive design

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -7,7 +7,7 @@ export default function Projects() {
       name: "Dad Jokes Finder",
       img: "./assets/dad.png",
       desc:
-        "Small side project I created to learn React functional components and hooks. This web app uses " +
+        "Tutuorial project I used to learn React functional components and hooks. This web app uses " +
         "the icanhazdadjoke.com web API to provide dad-joke-related search results.",
       link: "https://tydan3.github.io/Dad-Jokes-Finder/",
     },
@@ -15,21 +15,21 @@ export default function Projects() {
       id: 2,
       name: "Realm Defense Force",
       img: "./assets/rdf.png",
-      desc: "This is a Tower Defense web game, called Realm Defense Force. This was a group project for a web game programming course.",
+      desc: "This is a Tower Defense web game, called Realm Defense Force. This was a quarter-long project for a web game programming course.",
       link: "https://tydan3.github.io/Realm-Defense-Force/",
     },
     {
       id: 3,
       name: "T4G2",
       img: "./assets/t4g2.png",
-      desc: "This is an Android app, called T4G2. It is primarily a messaging app. It was a Android Studio group project for the Mobile Programming course.",
-      link: "https://github.com/tydan3/Android-App-T4G2",
+      desc: "This is an Android app. It is primarily a messaging app. It was developed for a mobile programming course.",
+      link: "https://youtu.be/HdQHP8s3pQ0",
     },
     {
       id: 4,
       name: "PC Builder",
       img: "./assets/pcbuilder.png",
-      desc: "This is my database project, PC Builder, for the Database Systems Design course.",
+      desc: "This is my database project for a database systems design course.",
       link: "https://github.com/tydan3/Database-Project-PC-Builder",
     },
   ];
