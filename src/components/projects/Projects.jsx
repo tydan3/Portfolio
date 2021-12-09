510beb835e0b18f785b478e1fa3560d388af1742
Daniel Ty <tydaniel270@gmail.com>
2021-12-09 06:53:09 -0800

    edited background color and project descriptions

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -6,30 +6,28 @@ export default function Projects() {
       id: 1,
       name: "Dad Jokes Finder",
       img: "./assets/dad.png",
-      desc:
-        "Tutuorial project I used to learn React functional components and hooks. This web app uses " +
-        "the icanhazdadjoke.com web API to provide dad-joke-related search results.",
+      desc: "Dad joke search page. Made with React functional components and hooks as well as some CSS. Connects to a web api for the dad joke data.",
       link: "https://tydan3.github.io/Dad-Jokes-Finder/",
     },
     {
       id: 2,
       name: "Realm Defense Force",
       img: "./assets/rdf.png",
-      desc: "This is a Tower Defense web game, called Realm Defense Force. This was a quarter-long project for a web game programming course.",
+      desc: "This is a tower defense web game. This was a quarter-long project for a web game programming course. Made mainly with JavaScript.",
       link: "https://tydan3.github.io/Realm-Defense-Force/",
     },
     {
       id: 3,
       name: "T4G2",
       img: "./assets/t4g2.png",
-      desc: "This is an Android app. It is primarily a messaging app. It was developed for a mobile programming course.",
+      desc: "This is an Android message app. It was developed for a mobile programming course. Made with Java in Android Studio for front-end and Node.js for back-end.",
       link: "https://youtu.be/HdQHP8s3pQ0",
     },
     {
       id: 4,
       name: "PC Builder",
       img: "./assets/pcbuilder.png",
-      desc: "This is my database project for a database systems design course.",
+      desc: "This is a relational database project for a database systems design course. Used mssql server for the database and Java Swing for the GUI. Allows users to create/save pc builds from a selection of parts on the database.",
       link: "https://github.com/tydan3/Database-Project-PC-Builder",
     },
   ];
