c3e046c4742dfc353685731c4fa05588142c9a64
Daniel Ty <tydaniel270@gmail.com>
2021-12-09 05:57:13 -0800

    finished responsive design

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,5 @@
 import "./topbar.scss";
-import { Person, Mail } from "@material-ui/icons";
+import { Mail } from "@material-ui/icons";
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
