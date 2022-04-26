66ba4794521f0aec6be1b750642ee002d31a663d
Daniel Ty <tydaniel270@gmail.com>
2022-04-26 11:03:21 -0700

    changed profile pic

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -20,7 +20,7 @@ export default function Intro() {
       <div className="left">
         <div className="circle">
           <div className="imgContainer">
-            <img src="assets/me.jpg" alt="me" />
+            <img src="assets/me2.jpg" alt="me" />
           </div>
         </div>
       </div>
